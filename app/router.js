386877b5e2e8780954a7ce6e61c5fe93257219e7
sagarrakshe <sagarrakshe2@gmail.com>
2015-03-16 19:39:10 +0530

    My initial commit

--- app/router.js
+++ app/router.js
@@ -6,6 +6,10 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('login', function() {
+    this.route('manager');
+    this.route('participant');
+  });
 });
 
 export default Router;
